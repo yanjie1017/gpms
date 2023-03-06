e0f3e2fd07f7b7fa4988d6b2317205ae8c9b1c60
Toh Yan Jie <yanjie20009@gmail.com>
2023-03-07 06:54:13 +0800

    Add hashing

--- src/services/HashingService.go
+++ src/services/HashingService.go
@@ -2,19 +2,31 @@ package services
 
 import (
 	"crypto/sha512"
+	"encoding/hex"
 	"fmt"
+	"hash/fnv"
+	"math/rand"
 	"strings"
+
+	model "src/models"
 
 	"golang.org/x/crypto/scrypt"
 )
 
-func GeneratePassword(siteInfo string, generationToken string, systemKey string, userInput string, length int) string {
+func GeneratePassword(request model.PasswordGenerationRequest, passwordInfo model.PasswordGenerationInfo, systemKey string) string {
+	var siteInfo string = passwordInfo.Metadata
+	var length int = int(passwordInfo.Length)
+
+	var generationToken string = request.GenerationToken
+	var userInput = request.UserInput
 
 	var salt []byte = generateSalt(siteInfo, generationToken, systemKey)
-	password, _ := scrypt.Key([]byte(userInput), salt, 16384, 8, 1, 32)
+	passwordBytes, _ := scrypt.Key([]byte(userInput), salt, 16384, 8, 1, 32)
+	var passwordString string = hex.EncodeToString(passwordBytes)
+	fmt.Println(passwordString)
 
-	var mappedPassword string = mapPassword(password, length)
-	fmt.Printf("scrypt:\t%x\n", mappedPassword)
+	var mappedPassword string = mapPassword(passwordString, length)
+
 	return mappedPassword
 }
 
@@ -28,12 +40,50 @@ func generateSalt(siteInfo string, generationToken string, systemKey string) []b
 	sha_512.Write([]byte(concatenatedString.String()))
 	var salt []byte = sha_512.Sum(nil)
 
-	fmt.Printf("sha512:\t%x\n", salt)
 	return salt
 }
 
-func mapPassword(password []byte, length int) string {
-	var mappedPassword string = string(password)
-	// TODO
-	return mappedPassword
+func toHashCode(s string) uint64 {
+	h := fnv.New64()
+	h.Write([]byte(s))
+	return h.Sum64()
+}
+
+func mapPassword(password string, requiredLength int) string {
+	var seed int64 = int64(toHashCode(password))
+
+	passwordBytes := []byte(password)
+
+	// Define the list of possible characters to include in the output string
+	upper := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	symbol := "~`! @#$%^&*()_-+={[}]|:;<,>.?/"
+
+	// Seed the random number generator with a fixed value to ensure consistent output
+	rand.Seed(seed)
+
+	originalLength := requiredLength / 2
+	upperLength := (requiredLength - originalLength) / 2
+	// symbolLength := requiredLength - originalLength - upperLength
+
+	output := make([]byte, requiredLength)
+
+	// Add characters from the hash to the output string
+	for i := 0; i < originalLength; i++ {
+		output[i] = passwordBytes[i]
+	}
+
+	for i := originalLength; i < originalLength+upperLength; i++ {
+		output[i] = upper[rand.Intn(len(upper))]
+	}
+
+	for i := originalLength + upperLength; i < requiredLength; i++ {
+		output[i] = symbol[rand.Intn(len(symbol))]
+	}
+
+	// Shuffle the output string to ensure randomness
+	rand.Shuffle(len(output), func(i, j int) {
+		output[i], output[j] = output[j], output[i]
+	})
+
+	return string(output)
 }
