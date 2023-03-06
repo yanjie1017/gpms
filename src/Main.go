7601a20acf299ae0dc5918ce0cc1d6d2de9173f7
Toh Yan Jie <yanjie20009@gmail.com>
2023-03-07 06:55:15 +0800

    Add main

--- src/Main.go
+++ src/Main.go
@@ -2,10 +2,45 @@ package main
 
 import (
 	"fmt"
-	service "src/services"
+	"net/http"
+
+	// service "src/services"
+	db "src/databases"
+	handler "src/handlers"
+
+	"github.com/gorilla/mux"
 )
+
+const portNumber = ":8000"
 
 func main() {
 	fmt.Println("Hello World")
-	service.GeneratePassword("a", "b", "c", "d", 1)
+	// service.GeneratePassword("a", "b", "c", "d", 1)
+	/* Connect to database */
+	dbConnection := db.ConnectDB()
+
+	/* Defer closing database connection */
+	defer dbConnection.DB.Close()
+
+	// entryTag := model.PasswordEntryTag{
+	// 	ClientId: 10000,
+	// 	EntryId:  10000,
+	// }
+	// // result, err := dbConnection.ListTables()
+	// result, err := dbConnection.RetrievePasswordInfo(entryTag)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(result.Length)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/v1/health", handler.HandleHealthCheck).Methods(http.MethodPost)
+	router.HandleFunc("/v1/password/generate", handler.HandlePasswordGeneration(dbConnection)).Methods((http.MethodPost))
+	router.HandleFunc("/v1/entry/create", handler.HandlePasswordEntryCreation(dbConnection)).Methods(http.MethodPost)
+
+	err := http.ListenAndServe(portNumber, router)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
